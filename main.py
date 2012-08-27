05f059ce5c3e3f4145ac40423ce52ec5ad8c046b
RomanZander <roman.zander@gmail.com>
2012-08-28 01:53:28 +0300

    listdir/stat

--- main.py
+++ main.py
@@ -10,7 +10,7 @@
 # TODO
 # ---------------------------------------------------------------------------------------------
 """
-    read config
+    raw list dir
 """
 # ---------------------------------------------------------------------------------------------
 # CHANGELOG
@@ -19,26 +19,62 @@
     ...
 '''
 import os
-import ConfigParser
+
+###
+import sys
+# import ConfigParser
+
+# cfgStorageRoot = "d:\\dev.Git\\pyAssetManagement\\test1"
+cfgStorageRoot = "d:\\dev.Git\\pySequenceTester\\test4"
+cfgSwitch = ( len( sys.argv ) > 1) ###
+
+
+varRawDirList = []
+varRawDirListInfo = []
+varSubDirList = []
+varRawFileList = []
 
 if __name__ == '__main__':
+    '''
     # configuration reading
     config = ConfigParser.RawConfigParser(allow_no_value=True)
     config.read('base.cfg')
     cfgDevice = str( config.get( 'storage', 'device' ))
     cfgPath = str( config.get( 'storage', 'path' ))
+    '''
+    if cfgSwitch: ###
+        cfgStorageRoot = sys.argv[1]
+    print 'cfgStorageRoot: ' + str( cfgStorageRoot ) ### 
+    print 'cfgSwitch: ' + str( cfgSwitch ) ###
+    print "------------" ###
     
-    ###
-    print cfgDevice
-    print cfgPath 
-    print "\n------------"
-    for rootdir, dirs, files in os.walk( cfgDevice + cfgPath ):
+    # read raw listing
+    try: 
+        varRawDirList = os.listdir( cfgStorageRoot )
+    except:
+        print "### Oops!: can't get listdir" ###
+        # TODO: log exeption
+        raise ValueError
+    
+    print 'len( varRawDirList ):\t' + str( len( varRawDirList )) + '\n' ###
+    
+    # collect stat info
+    for item in varRawDirList:
+        itemStat = os.stat( os.path.join( cfgStorageRoot, item ))
+        itemInfo = {}
+        itemInfo['path'] = cfgStorageRoot
+        itemInfo['name'] = item 
+        itemInfo['size'] = itemStat.st_size
+        varRawDirListInfo.append(itemInfo) 
+        
+    print 'len( varRawDirListInfo ):\t' + str( len( varRawDirListInfo )) + '\n' ###
+    print 'varRawDirListInfo[1]:\t' + str( varRawDirListInfo[1] ) + '\n' ###
+    
+    '''
+    for rootdir, dirs, files in os.walk( cfgStorageRoot ):
         print rootdir
         print dirs
         print files
         print '========'
-        
-    
-    
-
+    ''' 
     pass
