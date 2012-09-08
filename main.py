895e7f11eb048774810b4c94ed1af220d2767310
Roman Zander <roman.zander@gmail.com>
2012-09-08 21:46:17 +0300

    continued improvements

--- main.py
+++ main.py
@@ -11,6 +11,7 @@
 # ---------------------------------------------------------------------------------------------
 """
     raw list dir
+    qMessage "folder is gone"
 """
 # ---------------------------------------------------------------------------------------------
 # CHANGELOG
@@ -37,29 +38,28 @@ cfgSeqenceMediaExt = '.dpx', '.tif', '.jpg'
 cfgRePattern = '^(.*\D)?(\d+)?(\.[^\.]+)$' # modified '^(.*\D)?(\d+)?(\.[^\.]+)$'
 ### cfgReCompiled = re.compile( cfgRePattern, re.I ) 
 
-varRawDirList = []
-varRawDirListInfo = []
-varSubDirList = []
-varFileList = []
+varRawDirList = [] # list for folder's items
+varRawDirListInfo = [] # list for folder's items info
+varSubDirList = [] # list for folder's subfolders
+varFileList = [] # list for folder's files
 
 # let's read raw directory listing
 def getRawDirList( RootFolder ):
     try: 
         rawDirList = os.listdir( RootFolder )
     except:
-        print "### Oops!: can't get listdir \n\t" + RootFolder ###
         # TODO: log exception
-        # ValueError("Can't scan this folder: \n\t" + Folder)
-        raise ValueError 
+        # TODO: send exception message to MQ
+        print "### Oops!: folder is gone: \t", RootFolder ###
+        return False
         
     # TODO: log info listing
-    print 'len( rawDirList ):\t' + str( len( rawDirList )) + '\n' ###
+    print '### OK: items in folder ', RootFolder, ' : ', len( rawDirList ) ###
     
     return rawDirList
-    pass
 
 def getRawDirListInfo( RootFolder, FolderListing ):
-    rawDirListInfo = []
+    rawDirListInfo = [] # list for collected item's data
     # collect stat info
     for item in FolderListing:
         itemStat = os.stat( RootFolder + os.sep +  item ) 
@@ -71,41 +71,58 @@ def getRawDirListInfo( RootFolder, FolderListing ):
         itemInfo['size'] = itemStat.st_size
         itemInfo['mtime'] = itemStat.st_mtime
         rawDirListInfo.append( itemInfo )
-         
-    # TODO: log collected info    
-    print 'len( rawDirListInfo ):\t' + str( len( rawDirListInfo )) + '\n' ###
-    
     return rawDirListInfo
-    pass
+
+def sortOutCollected( rawDirListInfo ):
+    # init empty lists for folders / files
+    subDirList = [] 
+    fileList = []
+    # iterate and sort out
+    for itemInfo in rawDirListInfo:
+        # create output item dictionary
+        outItem = {'path':itemInfo['path'], 
+                   'name':itemInfo['name'], 
+                   'size':itemInfo['size'], 
+                   'mtime':itemInfo['mtime']}
+        # if item is folder append output item to folders list
+        if S_ISDIR( itemInfo['mode'] ):
+            subDirList.append( outItem )
+        # else if item is file append output item to files list
+        elif S_ISREG( itemInfo['mode'] ):
+            fileList.append( outItem )
+    # returns tuple of folders / files lists
+    return  ( subDirList, fileList )
 
 if __name__ == '__main__':
-
+    
+    
+    ### dev-only: select argument 
     if  ( len( sys.argv ) > 1 ): ###
         cfgStorageRoot = sys.argv[1] ###
     print '\n' + 'cfgStorageRoot: ' + str( cfgStorageRoot ) ### 
     print "------------" ###
+    ### /dev-only
     
     # get raw directory list 
     varRawDirList = getRawDirList( cfgStorageRoot )
-
+    # exit if something wrong
+    if varRawDirList == False:
+        exit( 0 ) # raise SystemExit with the 0 exit code.
     # get stat info about raw directory list items
     varRawDirListInfo = getRawDirListInfo( cfgStorageRoot, varRawDirList )
-
-    # sort out collected results
-    for itemInfo in varRawDirListInfo:
-        if S_ISDIR( itemInfo['mode'] ):
-            varSubDirList.append( itemInfo )
-        elif S_ISREG( itemInfo['mode'] ):
-            varFileList.append( itemInfo )
+    # sort out collected info to subfolders / files
+    varSubDirList, varFileList = sortOutCollected( varRawDirListInfo )
+    
+    
          
     print '\nlen( varSubDirList ):\t' + str( len( varSubDirList )) ###
     
     for item in varSubDirList:
-        #print item
+        print item
         pass 
     print '\nlen( varFileList ):\t' + str( len( varFileList )) ###
     for item in varFileList:
-        #print item
+        print item
         pass 
     
     pass
