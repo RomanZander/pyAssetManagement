c4f1b4c8943ea73987d4c89a5f0bee38ef15ead6
RomanZander <roman.zander@gmail.com>
2012-09-23 22:12:42 +0300

    0.0.3

--- scanResult.py
+++ scanResult.py
@@ -2,7 +2,7 @@
 '''
 @summary: AssetManagement scanResult
 @since: 2012.09.19
-@version: 0.0.3a
+@version: 0.0.3
 @author: Roman Zander
 @see:  https://github.com/RomanZander/pyAssetManagement
 '''
@@ -20,6 +20,7 @@
 # CHANGELOG
 # ---------------------------------------------------------------------------------------------
 '''
+    0.0.3 +process dipatcher
     0.0.3a + rabbitmq host
     0.0.2 +inbound processor
     0.0.1 +Initial commit
@@ -38,33 +39,44 @@ cfgRabbitInRoutingKey = 'scanResult_queue'
 cfgRabbitOutQueue = 'scanFolder_queue'
 cfgRabbitOutRoutingKey = 'scanFolder_queue'
 
+# status messages
+cfgFOLDERGONE = 'folderGone'
+cfgFOUNDSUBFOLDER = 'foundSubfolder'
+cfgNOSUBFOLDER = 'noSubfolder'
+cfgFOUNDFILE = 'foundFile'
+cfgNOFILE = 'noFile'   
+cfgFOUNDSEQUENCE = 'foundSequence'
+cfgNOSEQUENCE = 'noSequence'
+
+# set pika log level
 pika.log.setup(pika.log.INFO)
+#pika.log.setup(pika.log.ERROR)
 
 def inCallback(channel, method_frame, header_frame, body):
     # unpickle inbound
     data = cPickle.loads(body)
-    ### logged
+    # logged
     pika.log.info(
                 "Basic.Deliver %s delivery-tag %i: %s",
                 header_frame.content_type,
                 method_frame.delivery_tag,
                 data)
-    # call data processor
-    processIn(data) 
+    # call inbound data dispatcher
+    dispatchIn(data)
+    # send acknowledge 
     channel.basic_ack(delivery_tag = method_frame.delivery_tag)
     pass
 
-'''
-def handle_delivery(channel, method_frame, header_frame, body):
-    # Receive the data in 3 frames from RabbitMQ
-    pika.log.info("Basic.Deliver %s delivery-tag %i: %s",
-                  header_frame.content_type,
-                  method_frame.delivery_tag,
-                  body)
-    channel.basic_ack(delivery_tag=method_frame.delivery_tag)
-
-'''
-def processIn(data): # process inbound message
+def dispatchIn(data): # process inbound message
+    ###
+    print " [.] Inbound message:"
+    ###
+    
+    msgMessage = data['msgMessage']
+    if msgMessage == cfgFOUNDFILE:
+        print " [+] cfgFOUNDFILE", msgMessage
+    else:
+        print " [?] some else"
     
     ###
     print " [.] Processing inbound message..."
@@ -74,28 +86,20 @@ def processIn(data): # process inbound message
     pass
 
 if __name__ == '__main__':
-    
     # create RabbitMQ connection
     parameters = pika.ConnectionParameters(host = cfgRabbitHost)
     connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
+    channel = connection.channel() #default channel
     if connection.is_open:
-        ### log success
-        print ' [*] Waiting for messages. To exit press CTRL+C\n'
-        # MQ code here
-        channel.queue_declare(
-                              queue = cfgRabbitInQueue,
+        channel.queue_declare(queue = cfgRabbitInQueue,
                               durable = True)
-        # set qos
-        channel.basic_qos(prefetch_count=1)
-        # start In consuming
-        channel.basic_consume(
-                              inCallback,
+        channel.basic_qos(prefetch_count=1) # set qos
+        channel.basic_consume(inCallback, # set callback 
                               queue = cfgRabbitInQueue)
-        channel.start_consuming()
-        
-        ###
-        print "### consuming started"
+        # if logged
+        pika.log.info("Connection success, consuming start, queue: %s",
+                      cfgRabbitInQueue)
+        channel.start_consuming() # start In consuming
         pass
     else:
         ### log unsuccess connection
