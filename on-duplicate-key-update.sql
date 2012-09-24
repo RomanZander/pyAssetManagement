fe62721c800efe806df1ed633c2037ddf18b75c1
RomanZander <roman.zander@gmail.com>
2012-09-25 01:51:32 +0300

    latest

--- on-duplicate-key-update.sql
+++ on-duplicate-key-update.sql
@@ -1,7 +1,7 @@
-INSERT INTO `media` (`name`,`size`) 
-	VALUES 
-   ('a055',111),
-   ('a012',111)
-	ON DUPLICATE KEY UPDATE `size` = VALUES(`size`);
-# SELECT * FROM `media` LIMIT 0,100;
-SELECT  LEFT(`path`, 256),  `name`,  `size`,  `mtime`,  LEFT(`thumb`, 256),  LEFT(`comment`, 256),  `updated` FROM `media` ORDER BY `path` ASC, `name` ASC LIMIT 1000;
+INSERT INTO `media` (`path`,`name`,`size`) VALUES 
+ ('D:\\1','a333',111)
+ON DUPLICATE KEY UPDATE 
+`size` = VALUES(`size`),
+`name` = VALUES(`name`)
+;
+
