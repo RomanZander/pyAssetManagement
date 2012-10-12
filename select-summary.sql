591aed3ef4981d1f13cdfca85bb10155e3bd7d55
RomanZander <roman.zander@gmail.com>
2012-10-12 18:06:05 +0300

    sql test

--- select-summary.sql
+++ select-summary.sql
@@ -1,14 +1,9 @@
-select 
-t1.path as path1, t1.name as name1, t1.size as size1,
-t2.path as path, t2.name as name, t2.size as size, t2.mtime as mtime, t2.updated #, sum(t2.size) as sumsize
+select DISTINCTROW
+t1.path as path1,
+t2.path as path, t2.name as name, t2.size as size, t2.mtime as maxmtime, t2.updated as maxupdated
 FROM test.mediatest t1, test.mediatest t2
 where 
-(t2.path like concat(t1.path, '/%')) or
-((t2.path = t1.path) and (t2.name = t1.name))
-#GROUP by path1
-ORDER by path1
-
-
-
-
-
+(t2.path like concat(t1.path, '/', '%')) 
+or
+(t2.path = t1.path)
+order by path1, name
